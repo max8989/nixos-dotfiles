42b544b3215f413fdcaae4290fa749eb7982d339
Maxime Gagne <max.8989@hotmail.com>
2026-08-11 00:16:24 -0400

    change wallpaper

--- home/desktop.nix
+++ home/desktop.nix
@@ -216,7 +216,7 @@ in
       wallpaper = [
         {
           monitor = "";
-          path = "~/.config/backgrounds/nixos.png";
+          path = "~/.config/backgrounds/nixos-wallpaper.png";
         }
       ];
     };
